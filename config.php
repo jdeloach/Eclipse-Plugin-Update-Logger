6cd65785a25e502363d4381fd16a38a21a1c6767
Jordan DeLoach <jordan@jtmcgee.net>
2011-05-18 22:20:53 -0500

    no longer tracking config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbName = ""; // database name
-$dbUser = ""; // mysql user
-$dbServer = ""; // mysql server
-$dbPassword = ""; // mysql password
-
-$gAPI = ""; // google api key for maps 2.0
-?>
