0825d856bdc69cff1cf117a085f3aeaf622e4580
Jordan DeLoach <jordan@jtmcgee.net>
2011-05-19 21:36:41 -0500

    changed standard log to have new table style

--- lib/admin.class.php
+++ lib/admin.class.php
@@ -15,7 +15,7 @@ class Admin {
 		$array = $this->getLog();
 		print "<a href=\"lib/backupLog.class.php\">Download list as CSV ( Excel )</a><br />";
 		print "<a href=\"?clear\">Clear this List</a>";
-		print "<table style='background-color: #CDCDCD;' border='#FFF' width='700px'>\n";
+		print "<table width=\"700px\" border=\"1\" cellspacing=\"3\" cellpadding=\"3\" style='background-color: #CDCDCD;' border='#FFF'>\n";
 		print "<tr>\n";
 		print "<th width='4%'>#</th>\n";
 		print "<th width='19%'>IP</th>\n";
@@ -27,10 +27,16 @@ class Admin {
 		$loc = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
 		$loc = explode('?', $loc);
 		$url = $loc['0'];
+		$i = 0;
 		date_default_timezone_set('America/Chicago');
 		while ($d = mysql_fetch_array($array)) {
 			$date = (string) $d['date'];
-			print "<tr>\n";
+			if($i % 2)
+				$bgColor = "FFFFFF"; // color 1
+			else
+				$bgColor = "C0C0C0"; //color 2
+
+			print "<tr bgcolor='" . $bgColor  . "'>\n";
 			print "<td>" . $d['id'] . "</td>\n";
 			print "<td><a href='whois.php?ip=" . $d['ip'] . "'>" . $d['ip'] . "</a></td>\n";
 			print "<td>" . date('M j, Y g:i a', strtotime($date)) . "</td>\n";
@@ -38,6 +44,7 @@ class Admin {
 			print "<td><a href='lib/downloads.class.php?f=" . $d['feature'] . "'>" . $d['feature'] . "</td>\n";
 			print "<td><a href='?delete=" . $d['id'] . "'>Delete</a></td>\n";
 			print "</tr>\n";
+			$i++;
 		}
 		print "</table>";
 	}
