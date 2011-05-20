fa66789c26d307d0f196d620fab1ec3cf31d3480
Jordan DeLoach <jordan@jtmcgee.net>
2011-05-19 22:56:33 -0500

    revamped install to setup config.php

--- install.php
+++ install.php
@@ -1,4 +1,25 @@
 <?php 
+$dbUser = readline("MySQL username:");
+
+$dbPassword = readline("MySQL password:");
+
+$dbServer = readline("MySQL server:");
+
+$dbName = readline("MySQL database name:");
+
+$gAPI = readline("gAPI key (not required, hit enter if skipping):");
+
+$fh = fopen("config.php", 'w') or die('Can\'t open/create file config.php');
+fwrite($fh, "<?php\r\n");
+fwrite($fh, '$dbUser = \'' . $dbUser . "';\r\n");
+fwrite($fh, '$dbPassword = \'' . $dbPassword . "';\r\n");
+fwrite($fh, '$dbServer = \'' . $dbServer . "';\r\n");
+fwrite($fh, '$dbName = \'' . $dbName . "';\r\n");
+fwrite($fh, '$gAPI  = \'' . $gAPI . "';\r\n");
+
+fclose($fh);
+
+
 $query = '
 CREATE TABLE IF NOT EXISTS `eclipseLog` (
   `id` int(9) NOT NULL auto_increment,
@@ -11,6 +32,7 @@ CREATE TABLE IF NOT EXISTS `eclipseLog` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 ';
 $drop = 'DROP TABLE eclipseLog';
+require('config.php');
 require('lib/mysql.class.php');
 $mysql = new Mysql();
 $mysql->connect()->selectDB();
