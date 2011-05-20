2a4aeb99eb72b1d682fc027d30ba9ed01c3f5bac
Jordan DeLoach <jordan@jtmcgee.net>
2011-05-20 10:47:22 -0500

    added the base error files that get installed during the install

---
+++ install/errorOff.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(E_ERROR);
+function handleError($errno, $errstr, $error_file, $error_line) {
+	$time = date('r');
+		
+	$to = 'spam@jtmcgee.net'; //TO
+	$subject = "Eclipse Update Site FAILURE";
+	$message = "To whom it may concern, \n";
+	$message .= "It would appear that the Update logger has failed.\n";
+	$message .= "It has done it\'s best to shut off the system, but no promises.\n";
+	$message .= "Reminder, to shut it off, just change the name of .htaccess!\n";
+	$message .= "Here is some data to prove it!\n";
+	$message .= "And the time was ".$time."\n";
+	$message .= "PHP\'s turn!:\n";
+	$message .= '$errno: '.$errno."\n";
+	$message .= '$errstr: '.$errstr."\n";
+	$message .= '$error_file: '.$error_file."\n";
+	$message .= '$error_line: '.$error_line."\n";
+	$message = wordwrap($message, 70);
+		
+	//mail($to, $subject, $message) or die('Life sucks... were broken and we can\'t even tell our Sysadmins about it... ') or die('Failed to mail');
+
+	// TODO mail not working....
+	print $message;
+	print "Problem identified, administrators have been emailed. Thank you.";
+}
+set_error_handler("handleError");
+?>
